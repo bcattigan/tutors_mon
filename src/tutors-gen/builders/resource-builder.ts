b8c3352b1fe465f7aba59d491d3ae8d5e1277907
bcattigan <20095670@mail.wit.ie>
2023-02-12 11:30:41 +0000

    Work complete up to: 12-02-2023
    Feature: x

--- src/tutors-gen/builders/resource-builder.ts
+++ src/tutors-gen/builders/resource-builder.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import path from "path";
-import { copyFileToFolder, findFirstMatchingString, findLastMatchingString, getFileName, getFileType } from "../utils/utils";
+import { copyFile, findFirstMatchingString, getFileName, getFileType } from "../utils/utils";
 import { assetTypes, LearningResource, loTypes } from "./lo-types";
 
 export const resourceBuilder = {
@@ -8,7 +8,7 @@ export const resourceBuilder = {
   root: "",
 
   getLoType(route: string): string {
-    let lotype = findFirstMatchingString(loTypes, route, this.root);
+    let lotype = findFirstMatchingString(loTypes, route);
     if (lotype == "book") {
       lotype = "lab";
     } else if (lotype == "") {
@@ -18,7 +18,14 @@ export const resourceBuilder = {
   },
 
   build(dir: string): LearningResource {
-    const tree: LearningResource = { courseRoot: this.root, route: dir, type: this.getLoType(dir), lrs: [], files: [], id: path.basename(dir) };
+    const tree: LearningResource = {
+      courseRoot: this.root,
+      route: dir,
+      type: this.getLoType(dir),
+      lrs: [],
+      files: [],
+      id: path.basename(dir)
+    };
     const files = fs.readdirSync(dir);
     if (files.length > 0) {
       for (const file of files) {
@@ -47,12 +54,13 @@ export const resourceBuilder = {
         const fileName = getFileName(file);
         const filePath = file.replace(lr.courseRoot, "");
         const destPath = `${dest}${filePath.replace(fileName, "")}`;
-        copyFileToFolder(file, destPath);
+        copyFile(file, destPath);
       }
     });
     lr.lrs.forEach((lr) => this.copyAssetFiles(lr, dest));
   },
+
   copyAssets(dest: string) {
     this.copyAssetFiles(this.lr, dest);
-  },
+  }
 };
