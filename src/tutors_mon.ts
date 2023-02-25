4c805f737acf01815a4846398b0d13b1954f8e25
bcattigan <20095670@mail.wit.ie>
2023-02-25 22:59:38 +0000

    Work complete up to: 25-02-2023
    Feature: add_watcher

--- src/tutors_mon.ts
+++ src/tutors_mon.ts
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
+import * as fs from "fs";
+import chalk from "chalk";
+import chokidar from "chokidar";
 import { generate } from "./tutors-gen/tutors-gen";
 
-generate();
+if (fs.existsSync("course.md")) {
+  generate();
+  chokidar.watch(".", { ignored: "json", ignoreInitial: true, usePolling: true, interval: 100, binaryInterval: 300 }).on("all", (event, path) => {
+    console.log(`${event}: ${path}`);
+    try {
+      generate();
+    } catch (err) {
+      console.log();
+    }
+  });
+} else {
+  console.log(chalk.red("Cannot locate course.md. Please Change to course folder and try again."));
+}
